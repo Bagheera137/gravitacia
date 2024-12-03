c50617350fc53bbf96c5a5dc466aba11fceb1df5
Boss <kosevaa@mail.ru>
2024-12-03 19:07:32 +0300

    adfgh

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import wrap
 wrap.add_sprite_dir("mysprite")
 wrap.world.create_world(500,600)
 grass=wrap.sprite.add("texstures",250,600,"grass")
-pacman=wrap.sprite.add("pacman",200,-100,"player2")
+pacman=wrap.sprite.add("pacman",200,20,"player2")
 
 platform=wrap.sprite.add("mario-items",220,300,"moving_platform2")
 platform1=wrap.sprite.add("mario-items",150,360,"moving_platform2")
@@ -13,30 +13,39 @@ cacoyto_list=[platform,platform1]
 speed=0
 
 @wrap.always()
-def padenye(pos_x,pos_y):
+def padenye(pos_x,pos_y,keys):
     global speed
     wrap.world.set_title(pos_y)
     bottom=wrap.sprite.get_bottom(pacman)
 
 
-    if bottom<550:
+    if bottom<550 and speed<20:
         wrap.sprite.move(pacman,0,speed)
         speed=speed+2
-        bottom = wrap.sprite.get_bottom(pacman)
-        if bottom>550:
-            wrap.sprite.move_bottom_to(pacman,550)
-            speed=0
+    elif speed>=20:
+        speed=20
+        wrap.sprite.move(pacman, 0, speed)
+    if bottom>550:
+        wrap.sprite.move_bottom_to(pacman,550)
+        speed=0
     for i in cacoyto_list:
-        check_platform(i)
+        check_platform(i,keys)
 
-def check_platform(platform):
+def check_platform(platform,keys):
     global speed
     top_platform = wrap.sprite.get_top(platform)
     bottom = wrap.sprite.get_bottom(pacman)
     if bottom>top_platform and wrap.sprite.is_collide_sprite(pacman,platform):
         wrap.sprite.move_bottom_to(pacman, top_platform)
-        speed=-speed*0.8
+        speed = -7
+        if wrap.K_SPACE in keys:
+            speed = -20
+@wrap.on_key_always(wrap.K_LEFT,wrap.K_RIGHT)
+def move(keys):
+    global speed
+    if wrap.K_LEFT in keys:
+        wrap.sprite.move(pacman,-5,0)
+    elif wrap.K_RIGHT in keys:
+        wrap.sprite.move(pacman, +5, 0)
 
-@wrap.on_key_always(wrap.K_LEFT)
-def move():
-    wrap.sprite.move(pacman,-1,0)
+
