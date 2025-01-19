6acc2f784d918c48a3df2888df65137085f2c627
Boss <kosevaa@mail.ru>
2025-01-19 13:42:38 +0300

    adfgh

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import wrap
 
 def carta():
+    global pacman
     grass = wrap.sprite.add("texstures", 250, 600, "grass")
     platform_grass = wrap.sprite.add("mario-items", 250, 557, "moving_platform2", False)
     wrap.sprite.set_width(platform_grass, 500)
@@ -24,8 +25,8 @@ def carta():
     ____________________
     _x___x______________
     __x_________________
-    ____________________
-    ____________________
+    ________________0___
+    xxxxxxxxxxxxxxxxxxxx
     ____________________
     """
     for i in carta:
@@ -37,32 +38,16 @@ def carta():
         if i == "\n":
             line = line + 1
             col=0
+        if i=="0":
+            pix_x=col*50
+            pix_y=line*50
+            pacman = wrap.sprite.add("pacman",pix_x ,pix_y, "player2")
     print(cordinaty)
 
     for i in cordinaty:
         platform = wrap.sprite.add("mario-items", i[0], i[1], "moving_platform2")
         cacoyto_list.append(platform)
 
-
-
-
-
-@wrap.always()
-def padenye(pos_x,pos_y,keys):
-    global speed
-    wrap.world.set_title(pos_y)
-
-
-    wrap.sprite.move(pacman, 0, speed)
-    for i in cacoyto_list:
-        check_platform(i, keys)
-    speed = speed + 2
-
-    if speed>=20:
-        speed=20
-
-
-    move_pacman()
 def check_platform(platform,keys):
     global speed
     top_platform = wrap.sprite.get_top(platform)
@@ -76,19 +61,6 @@ def check_platform(platform,keys):
     elif speed<0 and wrap.sprite.is_collide_sprite(pacman,platform):
         wrap.sprite.move_top_to(pacman, bottom_platform+2)
 
-
-
-
-@wrap.on_key_always(wrap.K_LEFT,wrap.K_RIGHT)
-def move(keys):
-    global speed
-    right_platform=wrap.sprite.get_right(platform)
-    if wrap.K_LEFT in keys:
-        wrap.sprite.move(pacman,-5,0)
-        if wrap.sprite.is_collide_sprite(pacman,platform):
-            wrap.sprite.move_left_to(pacman,right_platform)
-    elif wrap.K_RIGHT in keys:
-        wrap.sprite.move(pacman, 5, 0)
 
 
 def move_camera(number_y,number_x):
@@ -109,10 +81,50 @@ def move_pacman():
 wrap.add_sprite_dir("mysprite")
 wrap.world.create_world(500,600)
 
-pacman=wrap.sprite.add("pacman",150,50,"player2")
 decorations = []
 cacoyto_list=[]
+pacman=None
+carta()
+
+
+
 
 speed=0
 
-carta()
+
+
+
+
+
+
+@wrap.on_key_always(wrap.K_LEFT,wrap.K_RIGHT)
+def move(keys):
+    global speed
+
+    if wrap.K_LEFT in keys:
+        wrap.sprite.move(pacman,-5,0)
+        for i in cacoyto_list:
+            if wrap.sprite.is_collide_sprite(pacman,i):
+                right_platform = wrap.sprite.get_right(i)
+                wrap.sprite.move_left_to(pacman,right_platform)
+    elif wrap.K_RIGHT in keys:
+        wrap.sprite.move(pacman, 5, 0)
+        for i in cacoyto_list:
+            if wrap.sprite.is_collide_sprite(pacman, i):
+                left_platform = wrap.sprite.get_left(i)
+                wrap.sprite.move_right_to(pacman, left_platform)
+
+@wrap.always()
+def padenye(pos_x,pos_y,keys):
+    global speed
+    wrap.world.set_title(pos_y)
+
+
+    wrap.sprite.move(pacman, 0, speed)
+    for i in cacoyto_list:
+        check_platform(i, keys)
+    speed = speed + 2
+
+    if speed>=20:
+        speed=20
+    move_pacman()
